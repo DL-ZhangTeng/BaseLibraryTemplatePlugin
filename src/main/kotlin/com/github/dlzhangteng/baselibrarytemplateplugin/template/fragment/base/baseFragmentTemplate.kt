d63efce6fd4a6d51b311f18e95abf98236ff4650
Swing <763263311@qq.com>
2022-01-05 15:28:42 +0800

    fragment、adapter、widget不加入NewProject、NewModule

--- src/main/kotlin/com/github/dlzhangteng/baselibrarytemplateplugin/template/fragment/base/baseFragmentTemplate.kt
+++ src/main/kotlin/com/github/dlzhangteng/baselibrarytemplateplugin/template/fragment/base/baseFragmentTemplate.kt
@@ -15,9 +15,7 @@ val baseFragmentTemplate
         formFactor = FormFactor.Mobile
         screens = listOf(
             WizardUiContext.FragmentGallery,
-            WizardUiContext.MenuEntry,
-            WizardUiContext.NewProject,
-            WizardUiContext.NewModule
+            WizardUiContext.MenuEntry
         )
 
         val mFragmentPackageName = stringParameter {
