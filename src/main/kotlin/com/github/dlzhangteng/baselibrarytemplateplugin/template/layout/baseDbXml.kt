c38aed26b833bbdcb90ceacfb2c60d1ad8c7f99d
Swing <763263311@qq.comm>
2022-12-06 10:44:42 +0800

    更改插件适配版本2.2.0

--- src/main/kotlin/com/github/dlzhangteng/baselibrarytemplateplugin/template/layout/baseDbXml.kt
+++ src/main/kotlin/com/github/dlzhangteng/baselibrarytemplateplugin/template/layout/baseDbXml.kt
@@ -5,7 +5,9 @@ fun baseDbXml(
     mBeanClass: String,
 ) = """
 <?xml version="1.0" encoding="utf-8"?>
-<layout xmlns:android="http://schemas.android.com/apk/res/android">
+<layout xmlns:android="http://schemas.android.com/apk/res/android"
+    xmlns:app="http://schemas.android.com/apk/res-auto"
+    xmlns:tools="http://schemas.android.com/tools">
 
     <data>
 
@@ -17,9 +19,7 @@ fun baseDbXml(
 
     <androidx.constraintlayout.widget.ConstraintLayout
         android:layout_width="match_parent"
-        android:layout_height="100dp"
-        android:layout_marginTop="10dp"
-        android:background="@color/base_theme_color">
+        android:layout_height="match_parent">
 
     </androidx.constraintlayout.widget.ConstraintLayout>
 </layout>
